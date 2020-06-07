b3eba62d2bcd6902981e5b0d95abae7e3de5872d
scope2229 <kaine_76@hotmail.com>
2020-06-07 14:40:25 +0800

    cleaned up help and fixed module core transform bug

--- core/help.rb
+++ core/help.rb
@@ -6,17 +6,29 @@ module Core
   # displays the help menu in terminal
   class Help
     def initialize
-      puts ''
-      puts 'Ruby shell terminal'
-      puts ''
-      puts 'Standard unix commands can be entered here including ability to pipe commands'
-      puts ''
-      puts 'Built in commands:'
-      puts ''
-      puts 'hist - Returns commands used in the current session.'
-      puts 'help - Displays the help info'
-      puts 'exit, quit, q - Exits the terminal'
-      puts ''
+      title
+      description
+      commands
+    end
+
+    def title
+      puts '',
+           ' Ruby Shell Terminal'
+    end
+
+    def description
+      puts '',
+           ' Standard unix commands can be entered here including ability to pipe commands',
+           ''
+    end
+
+    def commands
+      puts ' Built in commands:',
+           '',
+           '  hist - Returns commands used in the current session.',
+           '  help - Displays the help info',
+           '  exit, quit, q - Exits the terminal',
+           ''
     end
   end
 end
--- core/transform.rb
+++ core/transform.rb
@@ -3,9 +3,12 @@
 require_relative 'command'
 require_relative 'pipeline'
 
-class Transform < Parslet::Transform
-  # rule(left: subtree(:left), pipe: '|', right: subtree(:right)) { Pipeline.new(left, right) }
-  rule(command: sequence(:args)) { Command.new(args) }
-  rule(arg: simple(:arg)) { arg }
-  
+module Core
+  ##
+  # Core::Transform
+  class Transform < Parslet::Transform
+    # rule(left: subtree(:left), pipe: '|', right: subtree(:right)) { Pipeline.new(left, right) }
+    rule(command: sequence(:args)) { Command.new(args) }
+    rule(arg: simple(:arg)) { arg }
+  end
 end
